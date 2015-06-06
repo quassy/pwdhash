6e347c8b6e546ecfc5f4cf107fe36b87016d325e
quassy <quassy.github.io@gmail.com>
2015-06-06 03:36:56 +0200

    Rough layout, directory changes

--- web/pwdhash/pwdhash.js
+++ web/pwdhash/pwdhash.js
@@ -13,10 +13,9 @@
  * Initialize page with default hashing parameters.
  */
 function Init() {
-  document.hashform.domain.value = "http://www.example.com/";
+  document.hashform.domain.value = "";
   document.hashform.sitePassword.value = "";
-  document.hashform.hashedPassword.value = "Press Generate";
-  document.hashform.hashedPassword.disabled = true;
+  document.hashform.hashedPassword.value = "";
 }
 
 var SPH_kPasswordPrefix = "@@";
@@ -26,21 +25,11 @@ var SPH_kPasswordPrefix = "@@";
  */
 function Generate()
 {
-  var uri = document.hashform.domain.value;
-  var domain = (new SPH_DomainExtractor()).extractDomain(uri);
+  var domain = (new SPH_DomainExtractor()).extractDomain(document.hashform.domain.value);
   var size = SPH_kPasswordPrefix.length;
   var data = document.hashform.sitePassword.value;
   if (data.substring(0, size) == SPH_kPasswordPrefix)
     data = data.substring(size);
   var result = new String(new SPH_HashedPassword(data, domain));
-  return result;
-}
-
-/*
- * Obtain a conforming hashed password and put it in the hashed password field
- */
-function GenerateToTextField()
-{
-  document.hashform.hashedPassword.value = Generate();
-  document.hashform.hashedPassword.disabled = false;
+  document.hashform.hashedPassword.value = result;
 }
